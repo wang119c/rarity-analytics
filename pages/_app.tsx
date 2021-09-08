12b57308c2602e20c5f19721b8cd3c7970c40111
eabz <eabz@polispay.org>
2021-09-08 18:07:50 -0500

    Images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,54 +16,123 @@ const client = new ApolloClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-custom-background">
-      <header className="flex-shrink-0 w-full text-white">
-        <Popover
-          as="nav"
-          className="z-10 w-full bg-transparent header-border-b"
-        >
-          {({ open }) => (
-            <>
-              <div className="px-4 py-4">
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center uppercase">
-                    <a
-                      href={"/"}
-                      className="uppercase text-xl p-2 text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis md:p-3 whitespace-nowrap"
-                    >
-                      <h1>RARITY ANALYTICS</h1>{" "}
-                    </a>
-                  </div>
-                  <div className="fixed bottom-0 left-0 z-10 flex flex-row items-center justify-center w-full p-4 lg:w-auto bg-custom-background lg:relative lg:p-0 lg:bg-transparent">
-                    <a
-                      className="mx-2 text-3xl"
-                      href="https://twitter.com/RarityGame"
-                    >
-                      <FontAwesomeIcon icon={faTwitter} />
-                    </a>
-                    <a
-                      className="mx-2 text-3xl"
-                      href="https://discord.gg/NUrfGsUkmd"
-                    >
-                      <FontAwesomeIcon icon={faDiscord} />
-                    </a>
-                    <a
-                      className="mx-2 text-3xl"
-                      href="https://github.com/rarity-adventure"
-                    >
-                      <FontAwesomeIcon icon={faGithub} />
-                    </a>
+    <>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+      <title key="title">Rarity Game | Analytics</title>
+
+      <meta
+        key="description"
+        name="description"
+        content="Explore rarity"
+      />
+
+      <meta name="application-name" content="Rarity Game | Analytics" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+      <meta name="apple-mobile-web-app-title" content="Rarity Game | Analytics" />
+
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#3E4A94" />
+
+      <meta key="twitter:card" name="twitter:card" content="app" />
+      <meta key="twitter:title" name="twitter:title" content="Rarity Game | Analytics" />
+      <meta
+        key="twitter:url"
+        name="twitter:url"
+        content="https://analytics.rarity.game"
+      />
+      <meta
+        key="twitter:description"
+        name="twitter:description"
+        content="Explore rarity"
+      />
+      <meta
+        key="twitter:image"
+        name="twitter:image"
+        content="https://analytics.rarity.game/manifest-icon-192.png"
+      />
+      <meta
+        key="twitter:creator"
+        name="twitter:creator"
+        content="@RarityGame"
+      />
+      <meta key="og:type" property="og:type" content="website" />
+      <meta
+        key="og:site_name"
+        property="og:site_name"
+        content="Rarity Game | Analytics"
+      />
+      <meta key="og:url" property="og:url" content="https://analytics.rarity.game" />
+      <meta
+        key="og:image"
+        property="og:image"
+        content="https://analytics.rarity.game/apple-icon-180.png"
+      />
+      <meta
+        key="og:description"
+        property="og:description"
+        content="Explore rarity"
+      />
+      <div className="bg-custom-background">
+        <header className="flex-shrink-0 w-full text-white">
+          <Popover
+            as="nav"
+            className="z-10 w-full bg-transparent header-border-b"
+          >
+            {({ open }) => (
+              <>
+                <div className="px-4 py-4">
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center uppercase">
+                      <a
+                        href={"/"}
+                        className="uppercase text-xl p-2 text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis md:p-3 whitespace-nowrap"
+                      >
+                        <h1>RARITY ANALYTICS</h1>{" "}
+                      </a>
+                    </div>
+                    <div className="fixed bottom-0 left-0 z-10 flex flex-row items-center justify-center w-full p-4 lg:w-auto bg-custom-background lg:relative lg:p-0 lg:bg-transparent">
+                      <a
+                        className="mx-2 text-3xl"
+                        href="https://twitter.com/RarityGame"
+                      >
+                        <FontAwesomeIcon icon={faTwitter} />
+                      </a>
+                      <a
+                        className="mx-2 text-3xl"
+                        href="https://discord.gg/NUrfGsUkmd"
+                      >
+                        <FontAwesomeIcon icon={faDiscord} />
+                      </a>
+                      <a
+                        className="mx-2 text-3xl"
+                        href="https://github.com/rarity-adventure"
+                      >
+                        <FontAwesomeIcon icon={faGithub} />
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </>
-          )}
-        </Popover>
-      </header>
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    </div>
+              </>
+            )}
+          </Popover>
+        </header>
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </div>
+    </>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,20 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-      <div className="w-full mb-44">
+      <div className="w-full mb-16">
         <img alt="sword" src="/img/sword.png" className="mx-auto w-36" />
         <div className="w-full text-white text-center text-3xl uppercase">
           <h1>Rarity Analytics</h1>
         </div>
+        <div className="w-full text-white text-center text-xl mt-3">
+          <span>Real time information for the Rarity Game</span>
+        </div>
       </div>
-      <div className="w-full bg-custom-blue text-center pb-24"></div>
+      <div className="w-full bg-custom-blue text-center pb-24">
+        <div className="mx-auto w-3/5 border-white border-2 rounded-lg border-custom-green">
+
+        </div>
+      </div>
     </>
   );
 };
